2386096aa7acb70421ff984dbd95f6d8e8efe955
Marcos Di Paolo <mdipaolo@nerdwallet.com>
2024-08-21 16:52:12 -0300

    fix: gitignoring .env

--- src/connection.ts
+++ src/connection.ts
@@ -1,16 +1,18 @@
-import { DataSource } from "typeorm";
+import { DataSource, DataSourceOptions } from "typeorm";
 
-const connection = new DataSource({
+const dataSourceOptions: DataSourceOptions = {
   type: "mysql",
-  host: process.env.DB_HOST || "localhost",
-  port: parseInt(process.env.DB_PORT || "33060"),
-  username: process.env.DB_USER || "root",
-  password: process.env.DB_PASSWORD || "tstypeorm",
-  database: process.env.DB_NAME || "tstypeorm",
+  host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT),
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   entities: ["src/entities/*.ts"],
   logging: true,
   migrations: ["src/db/migrations/*.ts"],
   migrationsTableName: "migrations_meta",
-});
+};
+
+const connection = new DataSource(dataSourceOptions);
 
 export default connection;
