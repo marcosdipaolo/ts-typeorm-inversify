12397a7704905794c8644d80c30b2bc723d1c2c6
Marcos Di Paolo <mdipaolo@nerdwallet.com>
2024-08-21 00:40:55 -0300

    fix: gitignore

--- build/repositories/UserRepository.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var UserRepository = /** @class */ (function () {
-    function UserRepository() {
-    }
-    return UserRepository;
-}());
-exports.default = UserRepository;
